f50e98505b2a50a6a2ca7d4a4b7a0bc2888d21d2
Yuvi Pineni <ypineni@veracode.com>
2023-10-29 01:39:45 -0400

    add yaml changes

---
+++ policies/karpenter.tf
@@ -0,0 +1,64 @@
+variable "cluster_name" {
+  description = "Name of the cluster"
+  type        = string
+}
+
+variable "access_key" { }
+variable "secret_key" { }
+
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_iam_role" "karpenter_controller_role" {
+
+  name = "voltron_controller_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+}
+
+resource "aws_iam_role_policy" "karpenter_controller" {
+  name = "karpenter-policy-${var.cluster_name}"
+  role = aws_iam_role.karpenter_controller_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:CreateLaunchTemplate",
+          "ec2:CreateFleet",
+          "ec2:RunInstances",
+          "ec2:CreateTags",          
+          "iam:PassRole",
+          "ec2:TerminateInstances",
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeInstanceTypeOfferings",
+          "ssm:GetParameter"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
